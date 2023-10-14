1e7da626f1019682ed88e185de8f12158819dce5
MarjanCinnamon <marjan.stojanov@cinnamon.agency>
2023-10-14 20:57:12 +0200

    handle any csv parsing errors

--- backend/helpers.py
+++ backend/helpers.py
@@ -1,22 +1,26 @@
 from csv import reader
+from fastapi.exceptions import HTTPException
 
 
 def parse_csv(filename: str) -> list[dict]:
-    # TODO: add error handling
-
     formatted_data = []
-    with open(filename, newline="") as csv_file:
-        file_reader = reader(csv_file, delimiter=",")
-        next(file_reader)  # skip headers
+    try:
+        with open(filename, newline="") as csv_file:
+            file_reader = reader(csv_file, delimiter=",")
+            next(file_reader)  # skip headers
 
-        for row in file_reader:
-            formatted_data.append(
-                {
-                    "id": row[0],
-                    "name": row[1],
-                    "city": row[2],
-                    "country": row[3],
-                }
-            )
+            for row in file_reader:
+                formatted_data.append(
+                    {
+                        "id": row[0],
+                        "name": row[1],
+                        "city": row[2],
+                        "country": row[3],
+                    }
+                )
+    except Exception:
+        raise HTTPException(
+            status_code=400, detail=f"Missing or malformed CSV file: {filename}"
+        )
 
     return formatted_data
